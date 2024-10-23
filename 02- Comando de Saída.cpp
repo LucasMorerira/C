92f67bb2f96fca21a20e61ba4cf6f5fc08ba433a
LucasMorerira <francisco.lucasm19@gmail.com>
2024-10-23 11:11:21 -0300

    02- Comando de Saída.cpp

---
+++ 02- Comando de Saída.cpp
@@ -0,0 +1,13 @@
+#include  <iostream> // Incliuir a biblioteca padrão de entrada e saida
+
+int main ( ) {
+        std::cout << "nome: João Silva" << std::endl; //Impreime o nome
+        std::cout << "Contato: (11) 1234-5678" << std::endl; //Impreime o Contato
+        std::cout << "Email: joao.silva@gmail.com" << std::endl; //Impreime o Email
+        std::cout << "CPF: 123.456.789-00" << std::endl; //Impreime o CPF
+        std::cout << "Endereço; Rua exemplo , 123, São Paulo , SP" << std::endl; //Impreime o Endereço
+        std::cout << "Curso: Engenharia da computação" << std::endl; //Impreime o Curso
+        
+        return 0; //Indica que o programa terminou com sucesso
+}
+        
