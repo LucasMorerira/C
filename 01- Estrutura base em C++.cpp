aa6c499e19df232c03fe5a9311f6296d4fc313e1
LucasMorerira <francisco.lucasm19@gmail.com>
2024-10-23 11:10:47 -0300

    01- Estrutura base em C++.cpp

---
+++ 01- Estrutura base em C++.cpp
@@ -0,0 +1,6 @@
+#include <iostream> //Inclui a biblioteca padrão de entrada e saída
+
+int main ( ) { //Declaração d função principal do programa
+      std::cout << "olá mundo"; //Imprime "Hello Word" na tela 
+      return 0; //Indica que o programa terminou com sucesso
+}
